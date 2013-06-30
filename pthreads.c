e3ce830389274e86007a90a7db54061cbd1b2994
davidcunha <davidgoncalvescunha@gmail.com>
2013-06-30 17:00:00 +0100

    pthreads summation example

--- pthreads.c
+++ pthreads.c
@@ -9,4 +9,38 @@ void *runner(void *param); /* thread */
 
 int main(int argc, char *argv[])
 {
+  pthread_t tid; /* thread identifier */
+  pthread_attr_t attr; /* thread attributes */
+
+  if (argc != 2) {
+    fprintf(stderr, "usage: a.out <integer value>\n");
+    return -1;
+  }
+  if(atoi(argv[1] < 0)) {
+    fprintf(stderr, "%d must be >= 0\n", atoi(argv[1]));
+    return -1;
+  }
+
+  /* get the default attributes */
+  pthread_attr_init(&attr);
+  /* create the thread */
+  pthread_create(&tid,&attr,runner,argv[1]);
+  /* wait for the thread to exit */
+  pthread_join(tid,NULL);
+
+  printf("total = %d\n", sum);
+}
+
+void *runner(void *param)
+{
+  int i, upper = atoi(param);
+  sum = 0;
+
+  if (upper > 0) {
+    for (i = 1; i <= upper; i++) {
+      sum +=1;
+    }
+  }
+
+  pthread_exit(0);
 }
